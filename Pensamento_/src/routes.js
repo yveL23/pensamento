95631f93a0187d1e8b741446ebf7cf5100dab863
Levy Cordeiro <108874182+yveL23@users.noreply.github.com>
2023-10-16 10:07:46 -0300

    Add files via upload

--- Pensamento_/src/routes.js
+++ Pensamento_/src/routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const ThoughtsController = require("./controllers/ThoughtsController");
 const UsersController = require("./controllers/UsersController");
+const Thought = require("./model/Thought");
 
 const router = express.Router();
 
@@ -8,5 +10,12 @@ router.get("/user", UsersController.findAllUsers);
 router.get("/user/:id", UsersController.findUser);
 router.put("/users/:id", UsersController.updateUser);
 router.delete("/users/:id", UsersController.deleteUser);
+
+router.post("/thoughts", ThoughtsController.createThought);
+router.get("/thoughts", ThoughtsController.findAllThoughts);
+router.get("/thoughts/:id", ThoughtsController.findThoughts);
+router.delete("/thoughts/:id", ThoughtsController.deleteThoughts);
+router.put("/thought/:id", ThoughtsController.updateThought);
+
 
 module.exports = router;
--- Pensamento_/src/server.js
+++ Pensamento_/src/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = require("./routes");
 const dotenv = require("dotenv");
+const handlebars = require("express-handlebars");
+const app = express();
+
 dotenv.config();
 
 require("./database/index");
 
-const app = express();
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(router);
 
+app.get('/home', (req, res) =>{
+    return res.render('home')
+});
 
 app.listen(3333, console.log("Servidor on na porta 3333"));
