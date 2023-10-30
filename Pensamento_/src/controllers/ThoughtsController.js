094a3f0c078b35f76aaca3b85bd1204e742dfda0
Levy Cordeiro <108874182+yveL23@users.noreply.github.com>
2023-10-30 12:01:59 -0300

    Add files via upload

--- Pensamento_/src/controllers/ThoughtsController.js
+++ Pensamento_/src/controllers/ThoughtsController.js
@@ -2,17 +2,20 @@ const Thought = require("../model/Thought");
 
 module.exports = {
     async dashboard(req, res) {
-        return res.render("thoughts/dashboard");
+        const thoughts = await Thought.findAll({ raw: true });
+        return res.render("thoughts/dashboard", { thoughts });
     },
     
     async registerThought(req, res) {
         return res.render("thoughts/register");
     },
 
-    async findAllThoughts(req, res) {
-        const thoughts = await Thought.findAll({ raw: true});
-        
-        return res.json(thoughts)
+    async showPageEditThought(req, res) {
+        const { id } = req.params;
+
+        const thoughts = await Thought.findOne({ where: {id} })
+        const result = [thoughts.dataValues]
+        return res.render("thoughts/edit", { result });
     },
 
     async createThought(req, res)
@@ -20,22 +23,29 @@ module.exports = {
         const { title, description } = req.body;
         const thought = await Thought.create({title, description});
 
+        try{
+            if(thought) {
+                return res.redirect("/thoughts/dashboard");
+            }
+        }catch(error) {
+            console.error(error)
+        };
+
         return res.json(thought);
-    },
-
-    async findThoughts(req, res) {
-        const { id } = req.params;
-
-        const thought = await Thought.findOne({where:{id:id}});
-
-        return res.json(thought)
     },
 
     async deleteThoughts(req,res) {
         const {id} = req.params;
         const thought = await Thought.destroy({ where:  {id} });
 
-        return res.json({message: "Pensamento deletado"})
+        try{
+            if(thought) {
+                return res.redirect("/thoughts/dashboard");
+            }
+        }catch(error){
+            console.error(error)
+        }
+
     },
 
      async updateThought(req, res)
@@ -51,8 +61,13 @@ module.exports = {
         }
         );
 
-
-        return res.json(thought);
+        try{
+            if(thought) {
+                return res.redirect("/thoughts/dashboard");
+            }
+        }catch(error) {
+            console.error(error)
+        }
     }
 
 };
--- Pensamento_/src/routes/thoughtsRoutes.js
+++ Pensamento_/src/routes/thoughtsRoutes.js
@@ -6,10 +6,9 @@ const router = express.Router();
 
 router.get("/thoughts/dashboard", ThoughtsController.dashboard);
 router.get("/thoughts-create", ThoughtsController.registerThought);
-router.post("/thoughts", ThoughtsController.createThought);
-router.get("/thoughts", ThoughtsController.findAllThoughts);
-router.get("/thoughts/:id", ThoughtsController.findThoughts);
-router.delete("/thoughts/:id", ThoughtsController.deleteThoughts);
-router.put("/thought/:id", ThoughtsController.updateThought);
+router.post("/thoughts/create", ThoughtsController.createThought);
+router.post("/thoughts/remove/:id", ThoughtsController.deleteThoughts);
+router.get("/thoughts/edit/:id", ThoughtsController.showPageEditThought);
+router.post("/thoughts/update/:id", ThoughtsController.updateThought);
 
 module.exports = router;
